2e5a733592c432e1734b43d97c32a1c96c68071d
Panos-1993 <panoslampros@icloud.com>
2022-06-08 18:10:09 +0300

    commit

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,7 +4,6 @@ function Footer() {
     <p class="col-md-4 mb-0 text-muted">© 2022 Panagiots Lampros</p>
 
     <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-      <svg class="bi me-2" width="40" height="32"><use href="#bootstrap"></use></svg>
     </a>
 
     <ul class="nav col-md-4 justify-content-end">
